e70bda22f3366b85ee561fd2bea2f42653e2fa0b
gitdmnt <100859051+gitdmnt@users.noreply.github.com>
2023-07-09 04:36:04 +0900

    nanikore

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -16,7 +16,8 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learning React
+          Majidena
         </a>
       </header>
     </div>
