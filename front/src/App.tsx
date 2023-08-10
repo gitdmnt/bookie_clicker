a6334ffa334c1117ec8175d905c088b7651eca67
gitdmnt <100859051+gitdmnt@users.noreply.github.com>
2023-08-10 20:40:21 +0900

    frontの作業開始

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,24 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+
+type Props = {
+  sub: string
+}
+
+function App(props: Props) {
+  let subject = props.sub;
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Hello {subject}!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learning React
-          Majidena
-        </a>
       </header>
     </div>
   );
--- front/src/index.tsx
+++ front/src/index.tsx
@@ -9,7 +9,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <App sub="udachn" />
   </React.StrictMode>
 );
 
