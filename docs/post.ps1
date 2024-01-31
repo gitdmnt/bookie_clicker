93afb34fa5ed62c8b31f8b6c3104babe5a3334b8
Gitdmnt <100859051+gitdmnt@users.noreply.github.com>
2024-01-31 23:31:00 +0900

    消した

--- docs/post.ps1
+++
@@ -1,5 +0,0 @@
-$url = "http://localhost:3001/api/registerbook";
-$headers = @{
-    "Content-Type" = "application/json"
-    };
-Invoke-WebRequest $url -Method POST -Headers $headers -Body '{"user":0, "isbn": "9784588010590"}'
