9e9d4e18f718291f504536177c8a3c39d7bd7d1c
lactoneinfo <lactoneinfo@gmail.com>
2024-11-15 05:28:01 +0900

    feat: search ui

--- src/components/Main/Bookshelf/SearchWindow/search.tsx
+++ src/components/Main/Bookshelf/SearchWindow/search.tsx
@@ -9,10 +9,11 @@ export const Search = (props: { setStyle: any }) => {
   3. isbnが正しくない場合は、検索窓を赤く表示する。
   4. isbnが正しい場合は、検索窓を白く表示する。
   5. 取得した書籍情報を親に渡す。
-*/
+  */
 
-  // isbn
-  const [isbn, setIsbn]: [string, any] = useState("");
+  // isbn, title
+  const [isbn, setIsbn] = useState("");
+  const [title, setTitle] = useState("");
   const { setBookInfoContainer } = useBookContext();
 
   // 検索窓を赤くするためのスタイル
@@ -41,27 +42,34 @@ export const Search = (props: { setStyle: any }) => {
       .reduce(
         (acc: number, c: string, i: number) =>
           acc + parseInt(c) * (i % 2 === 0 ? 1 : 3),
-        0
-      );
+      0
+    );
     const checkDigit = (10 - (sum % 10)) % 10;
 
     i = i.slice(0, 12) + String(checkDigit);
 
     setIsbn(i);
+    setTitle("");
+  };
+
+  // タイトル検索窓を制御する
+  const handleTitleChange = (e: any) => {
+    setTitle(e.target.value);
+    setIsbn("");
   };
 
   // バックエンドを叩く
   const loadBookInfo = async () => {
-    if (isbn === "") {
+    if (!isbn && !title) {
       setBookInfoContainer([]);
       return;
     }
 
     const query: SruApiQuery = {
       operation: "searchRetrieve",
-      query: `isbn%3d${isbn}`,
+      query: isbn ? `isbn%3d${isbn}` : `title%3d${title}`,
       start_record: 1,
-      maximum_records: 5,
+      maximum_records: 10,
       record_packing: "xml",
       record_schema: "dcndl",
     };
@@ -78,17 +86,23 @@ export const Search = (props: { setStyle: any }) => {
     setSearchWindowStyle({ border: "none" });
   };
 
-  useEffect(() => {
-    loadBookInfo();
-  }, [isbn]);
-
   return (
     <div className="Search">
       <input
         type="text"
+        placeholder="タイトルを入力"
+        style={searchWindowStyle}
+        value={title}
+        onChange={(e) => handleTitleChange(e)}
+        disabled={!!isbn}
+      />
+      <input
+        type="text"
         placeholder="ISBNを入力"
         style={searchWindowStyle}
+        value={isbn}
         onChange={(e) => handleIsbnChange(e)}
+        disabled={!!title}
       />
       <button onClick={loadBookInfo}>検索</button>
     </div>
